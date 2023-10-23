0523fff917c199d8f66387b4120193df5de57ac3
perpetualgrimace <hello@jamesferrell.me>
2023-10-23 14:45:05 -0400

    updates readme & hides site from search engines

--- site/snippets/global.head.php
+++ site/snippets/global.head.php
@@ -7,6 +7,7 @@
 
   <meta charset="utf-8" />
   <meta name="viewport" content="width=device-width,initial-scale=1.0">
+  <meta name="robots" content="noindex" />
 
   <title><?= $site->title() ?> | <?= $page->title() ?></title>
 
