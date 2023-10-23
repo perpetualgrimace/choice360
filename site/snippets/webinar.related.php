9390e8949dc966d9af0e790ef652ec2699ee150b
perpetualgrimace <hello@jamesferrell.me>
2023-10-23 16:42:52 -0400

    generates static site

--- site/snippets/webinar.related.php
+++ site/snippets/webinar.related.php
@@ -2,21 +2,21 @@
 
   $webinars = $pages->find('librarianship/webinars')->children()->published();
 
-  $upcoming = $webinars->filterBy('date', '>', time());
-  $archived = $webinars->filterBy('date', '<', time());
+  // $upcoming = $webinars->filterBy('date', '>', time());
+  // $archived = $webinars->filterBy('date', '<', time());
 
-  // page template
-  if($page->date()->toDate() > time()):
-  $category = 'upcoming webinar';
+  // // page template
+  // if($page->date()->toDate() > time()):
+  // $category = 'upcoming webinar';
 
-  elseif($page->date()->toDate() < time()):
-  $category = 'archived webinar';
+  // elseif($page->date()->toDate() < time()):
+  // $category = 'archived webinar';
 
-  endif;
+  // endif;
 
   // upcoming webinars
-  $upcoming_items = $upcoming->sortBy('date')->flip()->not($page)->limit('4');
-  $archived_items = $archived->sortBy('date')->flip()->not($page)->limit('4');
+  // $upcoming_items = $upcoming->sortBy('date')->flip()->not($page)->limit('4');
+  // $archived_items = $archived->sortBy('date')->flip()->not($page)->limit('4');
 
 ?>
 
@@ -24,16 +24,19 @@
   <div class="columns u-left">
     <div class="column">
 
-        <?php if (($category == 'upcoming webinar') && ($upcoming_items->count() > 1)): ?>
+        <?php /* if (($category == 'upcoming webinar') && ($upcoming_items->count() > 1)): */ ?>
           <h2>More upcoming webinars:</h2>
 
           <div class="columns cards">
-            <?php foreach ($upcoming_items as $item): ?>
+            <?php
+              // foreach ($upcoming_items as $item):
+              foreach ($webinars as $item):
+            ?>
               <?php snippet('card', array('item' => $item)) ?>
             <?php endforeach; ?>
           </div>
 
-        <?php else: ?>
+        <?php /* else: ?>
           <h2>More from the archive:</h2>
 
           <div class="columns cards">
@@ -42,7 +45,7 @@
             <?php endforeach; ?>
           </div>
 
-        <?php endif ?>
+        <?php endif */ ?>
 
 
     </div>
