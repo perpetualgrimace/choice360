3d0f4cd4b07e3b944a3d21bc6789de33478ebbe0
perpetualgrimace <hello@jamesferrell.me>
2023-10-24 15:40:53 -0400

    removes references to missing content

--- site/snippets/librarianship.list.php
+++ site/snippets/librarianship.list.php
@@ -13,17 +13,8 @@
     <h3>Bibliographic Essays</h3>
     <?= $page->essays()->kirbytext() ?>
 
-	<h3>Authority File</h3>
-    <?= $page->podcast()->kirbytext() ?>
-
     <h3><?= $pages->find('blog')->headline() ?></h3>
     <?= $page->blog()->kirbytext() ?>
-
-    <h3>Outstanding Academic Titles</h3>
-    <?= $page->oat()->kirbytext() ?>
-
-	<h3>Choice Research</h3>
-	<?= $page->whitepaper()->kirbytext() ?>
 
   </div>
 </div>
