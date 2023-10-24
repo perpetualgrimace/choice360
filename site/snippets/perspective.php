cf6c861810a25fc6efea381fa01c588d487fc03f
perpetualgrimace <hello@jamesferrell.me>
2023-10-24 15:55:36 -0400

    fixes visual bug with perspective snippet
    hopefully for the last time

--- site/snippets/perspective.php
+++ site/snippets/perspective.php
@@ -8,13 +8,13 @@ if(isset($link)): $link = $link; else: $link = FALSE; endif;
 <figure class="perspective u-center">
   <?php if ($link != ''): ?>
 
-  <a class="perspective__link" href="<?= $link ?>">
+  <a class="perspective__link perspective__child" href="<?= $link ?>">
     <img class="perspective__img<?php if ($shadow === TRUE): echo ' has-shadow'; endif ?>" src="<?= $src->url() ?>" alt="">
   </a>
 
   <?php else: ?>
 
-  <img class="perspective__img<?php if ($shadow === TRUE): echo ' has-shadow'; endif ?>" src="<?= $src->url() ?>" alt="">
+  <img class="perspective__img perspective__child<?php if ($shadow === TRUE): echo ' has-shadow'; endif ?>" src="<?= $src->url() ?>" alt="">
 
   <?php endif ?>
 </figure>
